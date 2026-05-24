990f8987f5c0ce51da8b487b5afc8e04fbb140e0
Junaid101 <Junaid101@users.noreply.github.com>
2026-05-24 21:01:18 +0000

    deploy: 9da3c83739bf70c5a0d3801d3d546663a8190de4

---
+++ dev/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/volleyball-gamestats/dev/sw.js', { scope: '/volleyball-gamestats/dev/' })})}
